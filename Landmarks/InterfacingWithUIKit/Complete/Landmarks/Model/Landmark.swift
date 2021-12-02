8089a631a1371fa9e00d57d360af64ac6e9e3b1c
SpencerNinja <spencer.ninja@gmail.com>
2021-12-02 14:45:43 -0700

    finished section 1 of Interfacing With UIKit

--- Landmarks/InterfacingWithUIKit/Complete/Landmarks/Model/Landmark.swift
+++ Landmarks/InterfacingWithUIKit/Complete/Landmarks/Model/Landmark.swift
@@ -1,9 +1,9 @@
-//
-//  Landmark.swift
-//  Landmarks
-//
-//  Created by Spencer Hurd on 11/30/21.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A representation of a single landmark.
+*/
 
 import Foundation
 import SwiftUI
@@ -17,14 +17,14 @@ struct Landmark: Hashable, Codable, Identifiable {
     var description: String
     var isFavorite: Bool
     var isFeatured: Bool
-    
+
     var category: Category
     enum Category: String, CaseIterable, Codable {
         case lakes = "Lakes"
         case rivers = "Rivers"
         case mountains = "Mountains"
     }
-    
+
     private var imageName: String
     var image: Image {
         Image(imageName)
@@ -32,15 +32,14 @@ struct Landmark: Hashable, Codable, Identifiable {
     var featureImage: Image? {
         isFeatured ? Image(imageName + "_feature") : nil
     }
-    
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
-            longitude: coordinates.longitude
-        )
+            longitude: coordinates.longitude)
     }
-    
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
