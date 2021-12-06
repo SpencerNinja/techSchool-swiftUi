4935283689f77eca9b5d868d508a9d694a4209d3
SpencerNinja <spencer.ninja@gmail.com>
2021-12-06 15:51:16 -0700

    started Memorize app

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Spencer Hurd on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
