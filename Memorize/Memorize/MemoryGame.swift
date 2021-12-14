ff4d2c103c0f6ecfb3a74d05e6f55551f0d9d757
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-12-14 16:24:32 -0700

    1_11_36

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -12,19 +12,21 @@ struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+//        if let chosenIndex = index(of: card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
         print("\(cards)")
     }
     
-    func index(of card: Card) -> Int {
-        for index in 0..<cards.count {
-            if cards[index].id == card.id {
-                return index
-            }
-        }
-        return 0 // bogus
-    }
+//    func index(of card: Card) -> Int? {
+//        for index in 0..<cards.count {
+//            if cards[index].id == card.id {
+//                return index
+//            }
+//        }
+//        return nil
+//    }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
         cards = Array<Card>()
@@ -37,7 +39,7 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
