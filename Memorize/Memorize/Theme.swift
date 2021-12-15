32872e59e5f10eff4b105840e8b2d20142e7a245
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-12-15 15:03:06 -0700

    Initialized

---
+++ Memorize/Memorize/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by Spencer Hurd on 12/15/21.
+//
+
+import Foundation
+
+struct Theme {
+    var name: String
+    var emojis: [String]
+    var pairsOfCards: Int
+    var themeColor: String
+}
