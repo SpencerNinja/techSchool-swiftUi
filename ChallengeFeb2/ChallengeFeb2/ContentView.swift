c15a7fe9ff96860250988856e45e29197e0c70be
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-02-02 15:21:12 -0700

    refactored for memory usage

--- ChallengeFeb2/ChallengeFeb2/ContentView.swift
+++ ChallengeFeb2/ChallengeFeb2/ContentView.swift
@@ -31,76 +31,79 @@ struct ContentView: View {
         }
         
     }
-}
-
-struct QuadrantFrame: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(
-                width: UIScreen.main.bounds.width / 2,
-                height: UIScreen.main.bounds.height / 2
-            )
-            .border(Color.white, width: 3)
-    }
-}
-
-// 1st Quadrant
-struct movieImage: View {
-    var body: some View {
-        Image("WhatAboutBob")
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .modifier(QuadrantFrame())
-            .clipped()
-    }
-}
-
-// 2nd Quadrant
-struct movieShape: View {
-    var body: some View {
-        Circle()
-            .foregroundColor(Color.green)
-            .padding()
-            .shadow(color: Color.gray, radius: 10, x: 3, y: 3)
-            .modifier(QuadrantFrame())
-    }
-}
-
-// 3rd Quadrant
-struct movieLogo: View {
-    var body: some View {
-        Image(systemName: "brain.head.profile")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .symbolRenderingMode(.palette)
-            .foregroundStyle(.blue, .yellow)
-            .padding()
-            .modifier(QuadrantFrame())
-    }
-}
-
-// 4th Quadrant
-struct movieTitle: View {
-    var body: some View {
-        VStack {
-            Text("What About Bob?")
-                .font(.headline)
-                .foregroundColor(.blue)
-            Text("starring Bill Murray")
-                .font(.caption)
-                .foregroundColor(.yellow)
-            HStack {
-                Circle()
-                    .foregroundColor(.orange)
-                    .frame(width: 20, height: 20)
-                RoundedRectangle(cornerRadius: 5)
-                    .foregroundColor(.red)
-                    .frame(width: 40, height: 20)
-            }
+    
+    struct QuadrantFrame: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .frame(
+                    width: UIScreen.main.bounds.width / 2,
+                    height: UIScreen.main.bounds.height / 2
+                )
+                .border(Color.white, width: 3)
         }
-        .modifier(QuadrantFrame())
     }
+
+    // 1st Quadrant
+    struct movieImage: View {
+        var body: some View {
+            Image("WhatAboutBob")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .modifier(QuadrantFrame())
+                .clipped()
+        }
+    }
+
+    // 2nd Quadrant
+    struct movieShape: View {
+        var body: some View {
+            Circle()
+                .foregroundColor(Color.green)
+                .padding()
+                .shadow(color: Color.gray, radius: 10, x: 3, y: 3)
+                .modifier(QuadrantFrame())
+        }
+    }
+
+    // 3rd Quadrant
+    struct movieLogo: View {
+        var body: some View {
+            Image(systemName: "brain.head.profile")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .yellow)
+                .padding()
+                .modifier(QuadrantFrame())
+        }
+    }
+
+    // 4th Quadrant
+    struct movieTitle: View {
+        var body: some View {
+            VStack {
+                Text("What About Bob?")
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                Text("starring Bill Murray")
+                    .font(.caption)
+                    .foregroundColor(.yellow)
+                HStack {
+                    Circle()
+                        .foregroundColor(.orange)
+                        .frame(width: 20, height: 20)
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundColor(.red)
+                        .frame(width: 40, height: 20)
+                }
+            }
+            .modifier(QuadrantFrame())
+        }
+    }
+
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
